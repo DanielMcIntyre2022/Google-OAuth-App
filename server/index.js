5bf5e13f08df9ad34e933ed0ea8d647623f407c5
Daniel <daniel-mcintyre@hotmail.com>
2022-08-31 14:13:31 -0400

    updated server to include GoogleOAuth keys

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const passport = require('passport');
+const { googleClientSecret } = require('./config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    ClientSecret: keys.googleClientSecret
+}));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
