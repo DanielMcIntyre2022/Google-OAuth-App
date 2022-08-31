b86a5082cd003f6881b2fa6bec1a0deaa8c16e0f
Daniel <daniel-mcintyre@hotmail.com>
2022-08-31 16:04:05 -0400

    added access refresh token and profile infromation in call back response

--- server/index.js
+++ server/index.js
@@ -8,18 +8,25 @@ const app = express();
 passport.use(new GoogleStrategy(
 {
     clientID: keys.googleClientID,
-    ClientSecret: keys.googleClientSecret,
+    clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
 },
-    (accessToken) => {
-    console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile', profile);
 }));
 
 app.get(
     '/auth/google',
     passport.authenticate('google', {
     scope: ['profile', 'email']
-}));
+    }));
+
+app.get(
+    '/auth/google/callback',
+    passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
